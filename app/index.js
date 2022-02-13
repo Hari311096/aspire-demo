b1196ebe41c2ea60bfd7b6df156c46ed6008ab88
Hari <krishnanihari530@gmail.com>
2022-02-14 00:34:57 +0530

    [CORRECTION]: Status bar correction for android

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { ActivityIndicator, StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import colors from './config/colors';
 import App from './navigation/index';
 import configureStore from './redux/store';
 
@@ -11,7 +12,7 @@ export default function index() {
   return (
     <Provider store={store}>
       <PersistGate loading={<ActivityIndicator />} persistor={persistor}>
-        <StatusBar translucent backgroundColor="#fff" barStyle="dark-content" />
+        <StatusBar translucent backgroundColor={colors.primary} barStyle="light-content" />
         <App />
       </PersistGate>
     </Provider>
